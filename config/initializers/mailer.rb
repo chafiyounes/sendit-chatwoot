9e194e9d265d7de6fd16eb2ce403872c1a2a5ea4
youneschafi <148773350+youneschafi@users.noreply.github.com>
2026-02-26 11:06:21 +0000

    small fix with the mailer.rb

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -6,18 +6,11 @@ if ENV['BREVO_API_KEY'].present?
 end
 
 Rails.application.configure do
-  #########################################
-  # Configuration Related to Action Mailer
-  #########################################
-
-  # We need the application frontend url to be used in our emails
   config.action_mailer.default_url_options = { host: ENV['FRONTEND_URL'] } if ENV['FRONTEND_URL'].present?
-  # We load certain mailer templates from our database. This ensures changes to it is reflected immediately
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
-  # Config related to smtp
   smtp_settings = {
     address: ENV.fetch('SMTP_ADDRESS', 'localhost'),
     port: ENV.fetch('SMTP_PORT', 587)
@@ -34,31 +27,17 @@ Rails.application.configure do
   smtp_settings[:open_timeout] = ENV['SMTP_OPEN_TIMEOUT'].to_i if ENV['SMTP_OPEN_TIMEOUT'].present?
   smtp_settings[:read_timeout] = ENV['SMTP_READ_TIMEOUT'].to_i if ENV['SMTP_READ_TIMEOUT'].present?
 
-  config.action_mailer.delivery_method = :smtp unless Rails.env.test?
   config.action_mailer.smtp_settings = smtp_settings
-
-  # Use Brevo HTTP API if configured (overrides SMTP, works on Railway)
-  config.action_mailer.delivery_method = :brevo if ENV['BREVO_API_KEY'].present? && !Rails.env.test?
-
-  # Use sendmail if using postfix for email
-  config.action_mailer.delivery_method = :sendmail if ENV['SMTP_ADDRESS'].blank? && ENV['BREVO_API_KEY'].blank?
-
-  # You can use letter opener for your local development by setting the environment variable
   config.action_mailer.delivery_method = :letter_opener if Rails.env.development? && ENV['LETTER_OPENER']
+end
 
-  #########################################
-  # Configuration Related to Action MailBox
-  #########################################
-
-  # Set this to appropriate ingress service for which the options are :
-  # :relay for Exim, Postfix, Qmail
-  # :mailgun for Mailgun
-  # :mandrill for Mandrill
-  # :postmark for Postmark
-  # :sendgrid for Sendgrid
-  # :ses for Amazon SES
-  config.action_mailbox.ingress = ENV.fetch('RAILS_INBOUND_EMAIL_SERVICE', 'relay').to_sym
-
-  # Amazon SES ActionMailbox configuration
-  config.action_mailbox.ses.subscribed_topic = ENV['ACTION_MAILBOX_SES_SNS_TOPIC'] if ENV['ACTION_MAILBOX_SES_SNS_TOPIC'].present?
+# Set delivery method OUTSIDE the configure block so it isn't overridden
+unless Rails.env.test?
+  if ENV['BREVO_API_KEY'].present?
+    ActionMailer::Base.delivery_method = :brevo
+  elsif ENV['SMTP_ADDRESS'].present?
+    ActionMailer::Base.delivery_method = :smtp
+  else
+    ActionMailer::Base.delivery_method = :sendmail
+  end
 end
